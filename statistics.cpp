63e943f2a6f14defc3a4bfbfcc1829436ea0540b
portnoi85 <portnoi85@mail.ru>
2025-05-18 16:25:40 +0500

    Реализация стат. характеристик Min, Max, Mean, Std

--- statistics.cpp
+++ statistics.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <limits>
+#include <vector>
+#include <cmath>
 
 class IStatistics {
 public:
@@ -12,7 +14,7 @@ public:
 
 class Min : public IStatistics {
 public:
-	Min() : m_min{std::numeric_limits<double>::min()} {
+	Min() : m_min{std::numeric_limits<double>::max()} {
 	}
 
 	void update(double next) override {
@@ -33,12 +35,97 @@ private:
 	double m_min;
 };
 
+class Max : public IStatistics {
+public:
+	Max() : m_max{std::numeric_limits<double>::lowest()} {
+	}
+
+	void update(double next) override {
+		if (next > m_max) {
+			m_max = next;
+		}
+	}
+
+	double eval() const override {
+		return m_max;
+	}
+
+	const char * name() const override {
+		return "max";
+	}
+
+private:
+	double m_max;
+};
+
+class Mean : public IStatistics {
+public:
+	Mean() : m_count{0}, m_sum{0} {
+	}
+
+    void update(double next) override {
+		m_sum += next;
+		++m_count;
+	}
+
+    double eval() const override {
+		if (m_count == 0) {
+			return NAN;
+		}
+		return {m_sum/m_count};
+	}
+
+    const char* name() const override {
+		return "mean";
+	};
+
+private:
+    int m_count{0};
+    double m_sum{0};
+};
+
+class Std : public IStatistics {
+public:
+	Std() = default;
+
+    void update(double next) override {
+		values.push_back(next);
+	}
+
+    double eval() const override {
+		if (values.empty()) {
+			return NAN;
+		}
+		Mean mean_;
+		for (const double& val : values) {
+			mean_.update(val);
+		}
+		double mean = mean_.eval();
+		Mean disp;
+		for (const double& val : values) {
+			disp.update(std::pow(val - mean , 2));
+		}
+		return {std::sqrt(disp.eval())};
+	}
+
+    const char* name() const override {
+		return "std";
+	};
+
+private:
+	std::vector<double> values;
+};
+
+
 int main() {
 
-	const size_t statistics_count = 1;
+	const size_t statistics_count = 4;
 	IStatistics *statistics[statistics_count];
 
 	statistics[0] = new Min{};
+	statistics[1] = new Max{};
+	statistics[2] = new Mean{};
+	statistics[3] = new Std{};
 
 	double val = 0;
 	while (std::cin >> val) {
@@ -52,6 +139,7 @@ int main() {
 		std::cerr << "Invalid input data\n";
 		return 1;
 	}
+
 
 	// Print results if any
 	for (size_t i = 0; i < statistics_count; ++i) {
